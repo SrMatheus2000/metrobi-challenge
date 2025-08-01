8945bd66bdce3a624d7780cb0d5d0995c2fd713c
Matheus Wandscheer <matheus@wandscheer.dev>
2025-08-01 10:59:21 -0300

    move components to questions folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import FlexLayout from './FlexLayout.tsx'
-import ZenoParadoxAnimation from './Zeno.tsx'
+import FlexLayout from './questions/3_FlexLayout.tsx'
+import Zeno from './questions/5_Zeno.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FlexLayout />
-    <ZenoParadoxAnimation />
+    <Zeno />
   </StrictMode>,
 )
